1141dd10e0604ca80448b8ba093c1372bd72eed6
elhidarinouhayla <elhidarinouhayla@gmail.com>
2025-12-11 15:30:48 +0100

    Add Login page JSX with form, state, and router navigation

---
+++ frontend/app/Login/page.jsx
@@ -0,0 +1,74 @@
+'use client';
+import { useState } from 'react';
+// import { useRouter } from 'next/navigation';
+import Link from 'next/link';
+import styles from './login_styles.module.css';
+import { useRouter } from 'next/navigation'
+
+export default function LoginPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter();
+  
+
+  async function handleLogin(e) {
+    e.preventDefault();
+    
+    try {
+      const response = await fetch('http://127.0.0.1:8000/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify({ username, password })
+      });
+
+      if (!response.ok) {
+        alert("Username ou Password incorrect");
+        return;
+
+      }
+
+      const data = await response.json();
+
+      router.push("/analyze")
+      localStorage.setItem("token", data.token);
+
+      
+
+    } catch (error) {
+      console.error("Erreur:", error);
+      alert("Erreur interne");
+    }
+  }
+
+  return (
+    <div className={styles.container}>
+  <div className={styles.card}>
+    <h1 className={styles.title}>Login</h1>
+
+    <form onSubmit={handleLogin} className={styles.form}>
+        
+      <input
+        className={styles.input}
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={e => setUsername(e.target.value)}
+      />
+
+      <input
+        className={styles.input}
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+      />
+
+      <button type="submit" className={styles.btn}>
+        Login
+      </button>
+
+    </form>
+  </div>
+</div>
+  );
+}
